90edd6de1dcd39a9b1feb7357d20cf20f5c7504f
bernardomaccari <bernardomaccari2006@gmail.com>
2024-04-09 18:10:13 -0300

    api

---
+++ src/controller/taskController.js
@@ -0,0 +1,36 @@
+const connection = require('../config/db');
+
+async function storeTask(request, response) {
+    const params = Array(
+        request.body.id,
+        request.body.email,
+        request.body.nome
+    );
+
+    const query = "INSERT INTO tasks(id,email.nome) VALUE(?,?)";
+
+    connection.query(query, params, (err, results) => {
+        if (results) {
+            response
+                .status(201)
+                .json({
+                    success: true,
+                    message: "Sucesso!",
+                    data: results
+                });
+        } else {
+            response
+                .status(400)
+                .json({
+                    success: false,
+                    message: "deu merda",
+                    query: err.sql,
+                    sqlMessage: err.sqlMessage
+                })
+        }
+    })
+}
+
+module.exports = {
+    storeTask 
+}
