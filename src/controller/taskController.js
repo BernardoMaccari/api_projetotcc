3f765fa40192fff7527ea42407ef531274f10ac8
bernardomaccari <bernardomaccari2006@gmail.com>
2024-04-12 14:22:17 -0300

    projetotcc

--- src/controller/taskController.js
+++ src/controller/taskController.js
@@ -4,10 +4,12 @@ async function storeTask(request, response) {
     const params = Array(
         request.body.id,
         request.body.email,
-        request.body.nome
+        request.body.nome,
+        request.body.senha,
+        request.body.telefone
     );
 
-    const query = "INSERT INTO tasks(id,email.nome) VALUE(?,?)";
+    const query = "INSERT INTO usuario_site(id,email,nome,senha,telefone) VALUE(?,?,?,?,?)";
 
     connection.query(query, params, (err, results) => {
         if (results) {
