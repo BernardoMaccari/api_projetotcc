f2c874d8cd4f578ee7d40aa5ae785510fba5e276
bernardomaccari <bernardomaccari2006@gmail.com>
2024-07-05 17:20:09 -0300

    aaaa

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
+//importar módulo express
 const express = require("express");
 
+//importar módulo fileupload
+const fileupload = require("express-fileupload");
+
+//importar módulo dotenv
 const dotenv = require("dotenv").config();
 
 const userRouter = require('./routes/userRouter')
 
 const app = express();
+
+//habilitando o upload de arquivos
+app.use(fileupload());
+
+//rota de mandar
+app.post('/mandar', function(req, res) {
+    
+    console.log(req.body);
+    console.log(req.files.arquivo.name);
+    
+    req.files.arquivo.mv(__dirname+'/arquivos/'+req.files.arquivos.name);
+    res.end(); 
+});
 
 app.set("port", process.env.PORT || 3002);
 
